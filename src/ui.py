6f4bb68b8a9dc6a34ea65078d6a9b49d110a285a
Robert He <2567466856@qq.com>
2024-09-16 10:43:14 +0800

    labels updated

--- src/ui.py
+++ src/ui.py
@@ -538,11 +538,12 @@ def mk_ui():
 
     if not gvar.get("game_exist"):
         tk_messagebox.showwarning(title="提示", message="没有找到植物大战僵尸杂交版的游戏存档")
-    refresh_game_data(True, True)
-    resize_root()
 
     check_new_save_thread = Thread(target=checking_new_save, daemon=True)
     check_new_save_thread.start()
+    
+    refresh_game_data(False, True)
+    resize_root()
 
     root.mainloop()
 
